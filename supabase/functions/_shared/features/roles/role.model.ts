f9aa7063828e8e2c5cd8efff9a5b63e9fb0e29fc
Fitia Ismael RAKOTONDRAZAKA <ismaelrakotondrazaka@gmail.com>
2025-12-22 06:25:06 +0300

    fix: Update import paths in role.model.ts to include file extensions
    - Changed import statements in role.model.ts to specify '.ts' file extensions for better clarity and to prevent potential issues with module resolution.

--- supabase/functions/_shared/features/roles/role.model.ts
+++ supabase/functions/_shared/features/roles/role.model.ts
@@ -2,8 +2,8 @@
 // @ts-nocheck This file should not be checked
 import type { Enums } from '../../../../../types/database/schema.ts';
 
-import { Constants } from '../../../../../types/database/schema';
-import { createEnumConstants } from '../../utils/enums';
+import { Constants } from '../../../../../types/database/schema.ts';
+import { createEnumConstants } from '../../utils/enums.ts';
 
 export type Role = Enums<'role'>;
 
